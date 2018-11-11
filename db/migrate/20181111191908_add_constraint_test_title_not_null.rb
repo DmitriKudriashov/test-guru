cf38a36c2065acafb54fd9769ab676e01cb53073
DmitriKudriashov <kds.120731@gmail.com>
2018-11-11 21:39:14 +0200

    Constraints Not NULL

---
+++ db/migrate/20181111191908_add_constraint_test_title_not_null.rb
@@ -0,0 +1,5 @@
+class AddConstraintTestTitleNotNull < ActiveRecord::Migration[5.2]
+  def change
+  	change_column_null(:tests, :title, false)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_11_185210) do
+ActiveRecord::Schema.define(version: 2018_11_11_192535) do
 
   create_table "answers", force: :cascade do |t|
-    t.string "body"
+    t.string "body", null: false
     t.integer "user_id"
     t.integer "question_id"
     t.datetime "created_at", null: false
@@ -21,28 +21,28 @@ ActiveRecord::Schema.define(version: 2018_11_11_185210) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "body"
+    t.string "body", null: false
     t.integer "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "tests", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.integer "level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.string "login"
     t.string "password"
     t.datetime "created_at", null: false
