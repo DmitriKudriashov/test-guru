02df2b26078189efd012b951e281957b388aab32
DmitriKudriashov <kds.120731@gmail.com>
2018-11-21 19:43:53 +0200

    Add methods list_tests, list_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,4 +3,12 @@ class Test < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :user_tests, dependent: :destroy
   has_many :users, through: :user_tests
+
+  # Создайте метод класса в модели Test, который возвращает отсортированный
+  # по убыванию массив названий всех Тестов у которых Категория называется
+  # определённым образом (название категории передается в метод в качестве аргумента).
+  def self.list_by_category(category_name)
+    Test.joins('JOIN categories ON categories.id = tests.category_id')
+    .where(categories: {title: category_name}).order(title: :DESC).pluck(:title)
+  end
 end
