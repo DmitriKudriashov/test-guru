e9d60e347f3f596d21445da5b563d2dc2bf9edfc
DmitriKudriashov <kds.120731@gmail.com>
2018-11-25 00:27:47 +0200

    Rename all UserTest to TestsUser

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_112635) do
+ActiveRecord::Schema.define(version: 2018_11_24_220100) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -44,14 +44,15 @@ ActiveRecord::Schema.define(version: 2018_11_21_112635) do
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
-  create_table "user_tests", force: :cascade do |t|
+  create_table "tests_users", force: :cascade do |t|
     t.integer "value"
     t.integer "user_id"
     t.integer "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["test_id"], name: "index_user_tests_on_test_id"
-    t.index ["user_id"], name: "index_user_tests_on_user_id"
+    t.index ["test_id", "user_id"], name: "index_tests_users_on_test_id_and_user_id", unique: true
+    t.index ["test_id"], name: "index_tests_users_on_test_id"
+    t.index ["user_id"], name: "index_tests_users_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
