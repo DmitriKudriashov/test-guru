8a3051dbd9689405b1bc54ab9772dd1dc1385b95
DmitriKudriashov <kds.120731@gmail.com>
2018-11-26 12:31:34 +0200

    Add author to tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_24_220100) do
+ActiveRecord::Schema.define(version: 2018_11_25_153946) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2018_11_24_220100) do
     t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
