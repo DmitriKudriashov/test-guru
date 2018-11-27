16ed9e7b1863d313f5085cdb90428d734b464c85
DmitriKudriashov <kds.120731@gmail.com>
2018-11-27 22:51:26 +0200

    Change Diagram, default in tests_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_25_153946) do
+ActiveRecord::Schema.define(version: 2018_11_27_201923) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -47,9 +47,9 @@ ActiveRecord::Schema.define(version: 2018_11_25_153946) do
   end
 
   create_table "tests_users", force: :cascade do |t|
-    t.integer "value"
-    t.integer "user_id"
-    t.integer "test_id"
+    t.integer "value", default: 0
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["test_id", "user_id"], name: "index_tests_users_on_test_id_and_user_id", unique: true
