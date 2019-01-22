5381520ec1f6147d92aa90c48b361c5c6a11f914
DmitriKudriashov <kds.120731@gmail.com>
2019-01-22 13:27:15 +0200

    After verify (part13)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,11 @@ module ApplicationHelper
     link_to 'GitHub', "https://github.com/#{author}/#{repo}", target: '_blank'
   end
 
+  def flash_messages
+    flash.map do |key, message|
+      key = key == 'alert' ? 'danger' : 'info'
+      content_tag :div, message, class: "alert alert-#{key}"
+    end
+    .join("\n").html_safe
+  end
 end
