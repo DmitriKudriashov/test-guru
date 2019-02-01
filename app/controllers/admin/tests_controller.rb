96a5ba3e17358972082fa75782c07c3702a15118
DmitriKudriashov <kds.120731@gmail.com>
2019-02-01 10:20:32 +0200

    After Lesson: data-*Atributes

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -2,12 +2,12 @@
 
 class Admin::TestsController < Admin::BaseController
 
-  before_action :find_test, only: %i[show edit update destroy]
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[show edit update destroy update_inline]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
   def index
-    @tests = Test.all
   end
 
   def new
@@ -15,6 +15,14 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def edit; end
+
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_test_path
+    else
+      render :index
+    end
+  end
 
   def update
     if @test.update(test_params)
@@ -47,6 +55,9 @@ class Admin::TestsController < Admin::BaseController
 
   private
 
+  def set_tests
+    @tests = Test.all
+  end
   def find_test
     @test = Test.find(params[:id])
   end
