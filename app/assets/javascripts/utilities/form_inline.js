4762fadd57e424c88acc44bc2e94bf47dc1dd286
DmitriKudriashov <kds.120731@gmail.com>
2019-02-02 18:19:33 +0200

    Delete comments before git push

--- app/assets/javascripts/utilities/form_inline.js
+++ app/assets/javascripts/utilities/form_inline.js
@@ -11,22 +11,11 @@ document.addEventListener('turbolinks:load', function () {
     var resourceId = errors.dataset.resourceId
     formInlineHandler(resourceId)
   }
-
-  // $('.form-inline-link').on('click', formInlineLinkHandler)
-
-  // var errors = document.querySelector('.resource-errors')
-
-  // if (errors) {
-  //   var resourceId = errors.dataset.resourceId
-  //   formInlineHandler(resourceId)
-  // }
-
 })
 
 
 function formInlineLinkHandler(event) {
   event.preventDefault()
-
   var testId = this.dataset.testId
   formInlineHandler(testId)
 }
@@ -48,17 +37,4 @@ function formInlineHandler(testId) {
       link.textContent = 'Edit'
     }
   }
-
-  // var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
-  // var $testTitle = $('.test-title[data-test-id="' + testId + '"]')
-  // var $formInline = $('.form-inline[data-test-id="' + testId + '"]')
-
-  // $formInline.toggle()
-  // $testTitle.toggle()
-
-  // if ($formInline.is(':visible')){
-  //   link.textContent = 'Cancel'
-  // } else {
-  //     link.textContent = 'Edit'
-  // }
 }
