fd84834b8fbe45ef09dd8bde97beeaea65c2f977
DmitriKudriashov <kds.120731@gmail.com>
2019-02-05 10:35:16 +0200

    Replace host to d-kudriashov-test-guru.herokuapp.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test-guru_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'cryptic-depths-55669.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'd-kudriashov-test-guru.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
